cac117a73f23fdca0fc1141a724e99e56797d8cc
Dirken <ricardmeyerhofer@gmail.com>
2017-09-16 20:19:09 +0200

    updating a bit of front-end

--- website/website/ui.R
+++ website/website/ui.R
@@ -4,7 +4,21 @@ navbarPage(title="Fecal Matter predictor",
            tabsetPanel(id = "inTabset",
            tabPanel("Table",
                     p("This is tab 1"), 
-                    tags$div(class="right down",actionButton("tab1", "Next"))
+                    tags$div(
+                      tags$div(class="right down",actionButton("tab1", "Next")),
+                      tags$div(class="center", tags$ul(class="asdf",list(
+                        tags$li(img(class="small",src='table-active.png')),
+                        tags$li(class="middle", img(class="big-dot", src='dot.png')),
+                        tags$li(img(class="small",src='options.png')),
+                        tags$li(class="middle", img(class="big-dot", src='dot.png')),
+                        
+                        tags$li(img(class="small",src='omg1.png')),
+                        tags$li(class="middle", img(class="big-dot", src='dot.png')),
+                        
+                        tags$li(img(class="small",src='chart.png'))
+                        
+                      )))
+                    )
                     #          # Select filetype:
                     #          selectInput("readFunction", "Function to read data:", c(
                     #            # Base R:
@@ -53,6 +67,8 @@ navbarPage(title="Fecal Matter predictor",
                     #          mainPanel(
                     #            column(width = 12, d3tfOutput('table'))
                     #          )
+                    
+
            ),
            tabPanel("Scenario",
                 
@@ -65,27 +81,76 @@ navbarPage(title="Fecal Matter predictor",
                 
                 radioButtons("mollecular", label = "Do you want to use only mollecular variables?", choices = list("Yes" = 1, "No" = 2), 
                              selected = 1, inline = TRUE),
-                tags$div(class="left down",actionButton("tab21", "Previous")),
-                tags$div(class="right down",actionButton("tab22", "Next"))
+                
+                
+                tags$div(
+                  tags$div(class="left down",actionButton("tab21", "Previous")),
+                  tags$div(class="right down",actionButton("tab22", "Next")),
+                  tags$div(class="center", tags$ul(class="asdf",list(
+                    tags$li(img(class="small",src='table.png')),
+                    tags$li(class="middle", img(class="big-dot", src='dot.png')),
+                    tags$li(img(class="small",src='options-2.png')),
+                    tags$li(class="middle", img(class="big-dot", src='dot.png')),
+                    
+                    tags$li(img(class="small",src='omg1.png')),
+                    tags$li(class="middle", img(class="big-dot", src='dot.png')),
+                    
+                    tags$li(img(class="small",src='chart.png'))
+                    
+                  )))
+                )
               
            ),
 
            tabPanel("Modelling",
                     p("This is tab 3"),
-                    tags$div(class="left down",actionButton("tab31", "Previous")),
-                    tags$div(class="right down", actionButton("tab32", "Next"))
+                    tags$div(
+                      tags$div(class="left down",actionButton("tab31", "Previous")),
+                      tags$div(class="right down", actionButton("tab32", "Next")),
+                      tags$div(class="center", tags$ul(class="asdf",list(
+                        tags$li(img(class="small",src='table.png')),
+                        tags$li(class="middle", img(class="big-dot", src='dot.png')),
+                        tags$li(img(class="small",src='options.png')),
+                        tags$li(class="middle", img(class="big-dot", src='dot.png')),
+                        
+                        tags$li(img(class="small",src='omg.png')),
+                        tags$li(class="middle", img(class="big-dot", src='dot.png')),
+                        
+                        tags$li(img(class="small",src='chart.png'))
+                        
+                      )))
+                    )
+                    
            ),
            
            tabPanel("Results",p("This is tab 4"),
                     
-           tags$div(class="left down", actionButton("tab41", "Previous"))
+            tags$div(
+              tags$div(class="left down", actionButton("tab41", "Previous")),
+              tags$div(class="center", tags$ul(class="asdf",list(
+                tags$li(img(class="small",src='table.png')),
+                tags$li(class="middle", img(class="big-dot", src='dot.png')),
+                tags$li(img(class="small",src='options.png')),
+                tags$li(class="middle", img(class="big-dot", src='dot.png')),
+                
+                tags$li(img(class="small",src='omg1.png')),
+                tags$li(class="middle", img(class="big-dot", src='dot.png')),
+                
+                tags$li(img(class="small",src='chart-active.png'))
+               
+              )))
+            )
 
 
            )
            ),
             theme = "custom-bootstrap.css",
            
+           
 
+          #icon-table,  
+          #icon-stats, icon-charts, icon-pie-chart
+           
             tags$footer(class="web", tags$p(class="right", HTML("Developed by Ricard Meyerhofer Parra")))
           
 )
