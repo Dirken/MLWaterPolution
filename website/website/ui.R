5e86805e5a45590a68a0b2798ce151b032fa6594
Dirken <ricardmeyerhofer@est.fib.upc.edu>
2017-11-28 19:45:43 +0100

    workaround for the ui, now working fine

--- website/website/ui.R
+++ website/website/ui.R
@@ -19,51 +19,52 @@ dashboardPage(
   dashboardSidebar(
     sidebarMenu(id = "tab",
                 menuItem("Table", 
-                         tabName = "tab2",
-                         icon = icon("table"), 
-                         conditionalPanel(condition = "input.tab == tab1",
-                           selectInput("readFunction", "Format to read", c(
-                             # Base R:
-                             "read.table",
-                             "read.csv",
-                             "read.csv2",
-                             "read.delim",
-                             "read.delim2",
-                             
-                             # foreign functions:
-                             "read.spss",
-                             "read.arff",
-                             "read.dbf",
-                             "read.dta",
-                             "read.epiiinfo",
-                             "read.mtp",
-                             "read.octave",
-                             "read.ssd",
-                             "read.systat",
-                             "read.xport",
-                             "read_excel",
-                             
-                             # Advanced functions:
-                             "scan",
-                             "readLines"
-                           )
-                           ),
-                           
-                           # Argument selecter:
-                           htmlOutput("ArgSelect"),
-                           
-                           # Argument field:
-                           htmlOutput("ArgText"),
-                           
-                           # Upload data:
-                           fileInput("file", "Browse file"),
-                           htmlOutput('response2')
-                         )
+                         tabName = "tab1",
+                         icon = icon("table")
                          
+                                    
                 ),
                 menuItem("About", 
                          tabName = "tab2", 
                          icon = icon("info-circle")
+                ),
+                conditionalPanel("input.tab == 'tab1'",
+                                 selectInput("readFunction", "Format to read", c(
+                                   # Base R:
+                                   "read.table",
+                                   "read.csv",
+                                   "read.csv2",
+                                   "read.delim",
+                                   "read.delim2",
+                                   
+                                   # foreign functions:
+                                   "read.spss",
+                                   "read.arff",
+                                   "read.dbf",
+                                   "read.dta",
+                                   "read.epiiinfo",
+                                   "read.mtp",
+                                   "read.octave",
+                                   "read.ssd",
+                                   "read.systat",
+                                   "read.xport",
+                                   "read_excel",
+                                   
+                                   # Advanced functions:
+                                   "scan",
+                                   "readLines"
+                                 )
+                                 ),
+                                 
+                                 # Argument selecter:
+                                 htmlOutput("ArgSelect"),
+                                 
+                                 # Argument field:
+                                 htmlOutput("ArgText"),
+                                 
+                                 # Upload data:
+                                 fileInput("file", "Browse file"),
+                                 htmlOutput('response2')
                 )
                 
     )
@@ -74,16 +75,15 @@ dashboardPage(
     shinyjs::useShinyjs(),
     extendShinyjs(text = jsCode),
     tabItems(
-      sidebarPanel(
+      
         tabItem("tab1",
           DT::dataTableOutput("data")
-          
-        )
-      ),
+        ),
+
         
-      tabItem("tab2",
-        p("hola2")
-      )
+        tabItem("tab2",
+          p("hola2")
+        )
     )
   )
 )
