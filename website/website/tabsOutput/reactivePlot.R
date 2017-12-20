d920e592e8b6ca4d790ca1a3d02cb5d5b600ecc0
Dirken <ricardmeyerhofer@est.fib.upc.edu>
2017-12-20 13:30:59 +0100

    omw multiple plots

--- website/website/tabsOutput/reactivePlot.R
+++ website/website/tabsOutput/reactivePlot.R
@@ -1,63 +1,51 @@
  ################################
 # Reactive plot
 ################################
+ 
+ source("tabsOutput/dialog.R", local = TRUE)
+ 
 observeEvent(input$filename, {
   output$plot <- reactive({
-    print("dins reactiveplot")
-    for (i in 2:length(list.dirs(file.path(root, colnames(Dataset$data)[input$data_cell_clicked[2]$col+1])))){
-        
-      document <- read.csv(file.path(root, colnames(Dataset$data)[input$data_cell_clicked[2]$col+1])[i],
-                           sep="\t", 
-                           dec = ",",
-                           header = FALSE,
-                           strip.white = TRUE)
-      output$plot <- renderPlot({
-        glmOutput <- glm(formula = document[,1] ~ document[,2],data=document,family=poisson())
-        plot(glmOutput)
+    
+    max_plots <- length(list.dirs(file.path(root, colnames(Dataset$data)[input$data_cell_clicked[2]$col+1])))
+    
+    # Insert the right number of plot output objects into the web page
+    output$plot <- renderUI({
+      plot_output_list <- lapply(1:max_plots, function(i) {
+        plotname <- paste("plot", i, sep="")
+        plotOutput(plotname, height = 280, width = 250)
       })
       
+      # Convert the list to a tagList - this is necessary for the list of items
+      # to display properly.
+      do.call(tagList, plot_output_list)
+    })
+    
+    # Call renderPlot for each one. Plots are only actually generated when they
+    # are visible on the web page.
+    for (i in 2:max_plots) {
+      # Need local so that each item gets its own number. Without it, the value
+      # of i in the renderPlot() will be the same across all instances, because
+      # of when the expression is evaluated.
+      local({
+        my_i <- i
+        plotname <- paste("plot", my_i, sep="")
+        print((file.path(root, colnames(Dataset$data)[input$data_cell_clicked[2]$col+1])))
+        
+        document <- read.csv(list.files(file.path(root, colnames(Dataset$data)[input$data_cell_clicked[2]$col+1]))[my_i],
+                             sep="\t", 
+                             dec = ",",
+                             header = FALSE,
+                             strip.white = TRUE)
+        output[[plotname]] <- renderPlot({
+          glmOutput <- glm(formula = document[,1] ~ document[,2],data=document,family=poisson())
+          plot(glmOutput)
+        })
+
+      })
     }
-    print("----")
      
   })
 })
  
-
  
- # 
- # max_plots <- 5
- # 
- # shinyServer(function(input, output) {
- #   
- #   # Insert the right number of plot output objects into the web page
- #   output$plots <- renderUI({
- #     plot_output_list <- lapply(1:input$n, function(i) {
- #       plotname <- paste("plot", i, sep="")
- #       plotOutput(plotname, height = 280, width = 250)
- #     })
- #     
- #     # Convert the list to a tagList - this is necessary for the list of items
- #     # to display properly.
- #     do.call(tagList, plot_output_list)
- #   })
- #   
- #   # Call renderPlot for each one. Plots are only actually generated when they
- #   # are visible on the web page.
- #   for (i in 1:max_plots) {
- #     # Need local so that each item gets its own number. Without it, the value
- #     # of i in the renderPlot() will be the same across all instances, because
- #     # of when the expression is evaluated.
- #     local({
- #       my_i <- i
- #       plotname <- paste("plot", my_i, sep="")
- #       
- #       output[[plotname]] <- renderPlot({
- #         plot(1:my_i, 1:my_i,
- #              xlim = c(1, max_plots),
- #              ylim = c(1, max_plots),
- #              main = paste("1:", my_i, ".  n is ", input$n, sep = "")
- #         )
- #       })
- #     })
- #   }
- # })
