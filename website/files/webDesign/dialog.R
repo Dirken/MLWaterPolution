0dde382d5b483241d2609f885337de2fe9488f4d
Dirken <ricardmeyerhofer@est.fib.upc.edu>
2018-01-14 15:19:35 +0100

    uploading minor solved

--- website/files/webDesign/dialog.R
+++ website/files/webDesign/dialog.R
@@ -8,9 +8,9 @@ observeEvent(input$show, {
     
     sidebarPanel(
       prettyRadioButtons(inputId = "alreadyLoaded",
-                         label = "Do you want to only consider the uploaded file?",
+                         label = "Upload own file?",
                          choices = c("Yes","No"), icon = icon("check"), inline = TRUE,
-                         status = "primary", fill = TRUE,  bigger = TRUE, animation = "jelly", selected = "No"),
+                         status = "primary", fill = TRUE,  bigger = TRUE, animation = "jelly", selected = "Yes"),
       conditionalPanel(condition = "input.alreadyLoaded == 'No'",
                  prettyRadioButtons(inputId = "plotChooser",
                          label = "Which plot would you like?",
@@ -19,22 +19,26 @@ observeEvent(input$show, {
                  #uiOutput('select.folder'),
                  uiOutput('select.file')
       ),
+      conditionalPanel(condition = "input.alreadyLoaded == 'Yes'",
        prettyRadioButtons(inputId = "locationChooser",
                           label = "Slope function",
                           choices = c("T99","T90"), icon = icon("check"), inline = TRUE,
                           status = "primary", fill = TRUE,  bigger = TRUE, animation = "jelly"),
        numericInput(inputId = "slope", label = "Insert your slope:", value = 0, step = 0.01),
-       uiOutput("finalFunction"),
        div(class="right",
         actionBttn("submit","Add season",color = "primary", style = "jelly", icon = icon("plus"))
        ), br(),br()
+      )
     ),
     mainPanel(
       conditionalPanel(condition = "input.alreadyLoaded == 'No'",
         withSpinner(uiOutput("plot"))
       ),
-      HTML("<p style='font-weight: bold;'>Uploaded data</p>"),
-      withSpinner(plotOutput("uploaded"))
+      conditionalPanel(condition = "input.alreadyLoaded == 'Yes'",
+                       
+        HTML("<p style='font-weight: bold;'>Uploaded data</p>"),
+        withSpinner(plotOutput("uploaded"))
+      )
     ),
     easyClose = TRUE,
     footer = tagList(actionButton("saveModal",label = "Save"),
@@ -46,12 +50,13 @@ observeEvent(input$show, {
 })
 observeEvent(input$saveModal,{
   #hauria de guardar el slope. Per altra part, quan introdueixo les dades, hauria de guardar l'slope tal qual i no ho faig atm...
-  conditionalPanel(condition = "input.alreadyLoaded == 'Yes'",
+  print("El valor de input$already loaded es : ")
+  print(input$alreadyLoaded)
+  if(input$alreadyLoaded == 'No'){
     Dataset$data[input$data_cell_clicked[1]$row+1, input$data_cell_clicked[2]$col+1] <- input$filename 
-  )
-  conditionalPanel(condition = "input.alreadyLoaded == 'No'",
-                   Dataset$data[input$data_cell_clicked[1]$row+1, input$data_cell_clicked[2]$col+1] <- "Custom input" 
-  )
+  }else{
+    Dataset$data[input$data_cell_clicked[1]$row+1, input$data_cell_clicked[2]$col+1] <- "Custom input" 
+  }
   
   removeModal()
 })
