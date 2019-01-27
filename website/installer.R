d275799f4a63e3971fce5f7dba0b35fcc583cd8a
RMeyerhofer <ricard.meyerhofer@est.fib.upc.edu>
2019-01-28 00:55:59 +0100

    installer

---
+++ website/installer.R
@@ -0,0 +1,25 @@
+#Installer:
+
+install.packages(c("shiny",
+                   "foreign",
+                   "htmlwidgets",
+                   "rhandsontable",
+                   "shinydashboard",
+                   "DT",
+                   "leaflet",
+                   "shinyjs",
+                   "ggplot2",
+                   "dplyr",
+                   "scales",
+                   "shinyFiles",
+                   "shinyBS",
+                   "shinyjqui",
+                   "shinycssloaders",
+                   "shinyWidgets",
+                   "gridExtra",
+                   "V8")
+)
+
+#library(D3TableFilter)
+
+#library(googlesheets)
